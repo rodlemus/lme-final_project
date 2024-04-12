493b0d37e1fc7d4114ec7fcff688a6dd904a4ab2
Call Center <rodlemus30@gmail.com>
2024-04-12 10:19:40 -0600

    feat: contact us ready

---
+++ public/js/meet-us/contact.js
@@ -0,0 +1,27 @@
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 13.676746900226256, lng: -89.25471536457081 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 15,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
